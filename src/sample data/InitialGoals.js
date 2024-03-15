933be8aa697092606edc23252877ba1334b67f0a
Alex <K1921081@kingston.ac.uk>
2024-03-15 13:01:24 +0000

    Add some sample data for testing the app

---
+++ src/sample data/InitialGoals.js
@@ -0,0 +1,32 @@
+export const goals = [
+    {
+        title: "Complete the ListScreen",
+        description: "Create the ListScreen screen for the Mobile Application Development assignment Major Mobile Application",
+        progress: 0,
+        goal: 1
+    },
+    {
+        title: "Complete the AddScreen",
+        description: "Create the AddScreen screen for the Mobile Application Development assignment Major Mobile Application",
+        progress: 0,
+        goal: 1
+    },
+    {
+        title: "Complete the EditScreen",
+        description: "Create the EditScreen screen for the Mobile Application Development assignment Major Mobile Application",
+        progress: 0,
+        goal: 1
+    },
+    {
+        title: "Complete the Major Mobile Application  assignment",
+        description: "Create the Checkpoint app for the Major Mobile Application assignment",
+        progress: 0,
+        goal: 1
+    },
+    {
+        title: "Complete weekly report MAD (15th Mar)",
+        description: "Complete the progress and planning report for the Mobile Application Development subject for 15th of March.",
+        progress: 0,
+        goal: 1
+    },
+];
